81914d4b47e359747ab47b331663d6cf41073519
AlexSwiss <39837597+AlexSwiss@users.noreply.github.com>
2019-09-28 01:59:35 +0100

    resolved bugs, uploaded code

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Chat from './component/Chat';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Chat/>
+    </div>
+  );
+}
+
+export default App;
---
+++ server.js
@@ -0,0 +1,37 @@
+    // server.js
+
+    require('dotenv').config({ path: 'variables.env' });
+
+    const express = require('express');
+    const bodyParser = require('body-parser');
+    const cors = require('cors');
+    const path = require('path');
+
+
+    const processMessage = require('./process-message');
+
+    const app = express();
+
+    app.use(cors());
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
+
+
+    app.post('/chat', (req, res) => {
+      const { message } = req.body;
+      processMessage(message);
+    });
+
+  if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+// Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
+
+    app.set('port', process.env.PORT || 5000);
+    const server = app.listen(app.get('port'), () => {
+      console.log(`Express running → PORT ${server.address().port}`);
+    });
