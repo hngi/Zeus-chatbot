74c9e817a11f73badb8bb321ca87c81d707fad64
AlexSwiss <39837597+AlexSwiss@users.noreply.github.com>
2019-09-29 10:09:43 +0100

    solved CORS error

--- server.js
+++ server.js
@@ -12,7 +12,19 @@
 
     const app = express();
 
-    app.use(cors());
+    // Set up a whitelist and check against it:
+var whitelist = ['http://api.openweathermap.org']
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
+    app.use(cors(corsOptions));
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
 
