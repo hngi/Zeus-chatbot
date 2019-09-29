72b4ec0fea0368c4a7c502ed085913dd1ac130b5
AlexSwiss <39837597+AlexSwiss@users.noreply.github.com>
2019-09-29 12:54:34 +0100

    solved the header bug

--- server.js
+++ server.js
@@ -17,11 +17,6 @@
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
 
-    app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    next();
-  });
-
 
     app.post('/chat', (req, res) => {
       const { message } = req.body;
