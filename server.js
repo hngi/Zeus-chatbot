1c26527c3ad245ff179358781edfbab661dae906
AlexSwiss <39837597+AlexSwiss@users.noreply.github.com>
2019-09-29 10:23:53 +0100

    added res.header

--- server.js
+++ server.js
@@ -16,11 +16,11 @@
     app.use(cors());
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
-    app.use(function(req, res, next) {
-      res.header("Access-Control-Allow-Origin", "*");
-      res.header("Access-Control-Allow-Headers", "X-Requested-With");
-      next();
-      });
+
+    app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+  });
 
 
     app.post('/chat', (req, res) => {
